f537e1c35523c265b946e4e0fc04fb828df60cf4
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-11 17:19:07 +0530

    added routes to all components

--- src/App.js
+++ src/App.js
@@ -4,24 +4,33 @@ import Home from './scenes/Home.js';
 import LuxuryHotels from './scenes/luxuryHotels/LuxuryHotels';
 import SmallHotels from './scenes/smallHotels/SmallHotels';
 import TopHotels from './scenes/topHotels/TopHotels';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 const App = () =>
   <Router>
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/topHotels">Top Hotels</Link>
-        </li>
-        <li>
-          <Link to="/smallHotels">Small Hotels</Link>
-        </li>
-        <li>
-          <Link to="luxuryHotels">Luxury Hotels</Link>
-        </li>
-      </ul>
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>World Hotel reviews</Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <NavItem eventKey={1}>
+              <Link to="/">Home</Link>
+            </NavItem>
+            <NavItem eventKey={2}>
+              <Link to="/topHotels">Top Hotels</Link>
+            </NavItem>
+            <NavItem eventKey={3}>
+              <Link to="/luxuryHotels">Luxury Hotels</Link>
+            </NavItem>
+            <NavItem eventKey={4}>
+              <Link to="/smallHotels">Small Hotels</Link>
+            </NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Route exact={true} path="/" component={Home} />
       <Route path="/luxuryHotels" component={LuxuryHotels} />
       <Route path="/smallHotels" component={SmallHotels} />
