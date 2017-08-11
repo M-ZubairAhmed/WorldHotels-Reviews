76542aa903117dadd017472a1cc15737ec8efcf1
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-11 16:20:48 +0530

    router and routes added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-class App extends Component {
-  render() {
-    return <div />;
-  }
-}
+import Home from './scenes/Home.js';
+import LuxuryHotels from './scenes/luxuryHotels/LuxuryHotels';
+import SmallHotels from './scenes/smallHotels/SmallHotels';
+import TopHotels from './scenes/topHotels/TopHotels';
+
+const App = () =>
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/topHotels">Top Hotels</Link>
+        </li>
+        <li>
+          <Link to="/smallHotels">Small Hotels</Link>
+        </li>
+        <li>
+          <Link to="luxuryHotels">Luxury Hotels</Link>
+        </li>
+      </ul>
+      <Route exact={true} path="/" component={Home} />
+      <Route path="/luxuryHotels" component={LuxuryHotels} />
+      <Route path="/smallHotels" component={SmallHotels} />
+      <Route path="/topHotels" component={TopHotels} />
+    </div>
+  </Router>;
 
 export default App;
