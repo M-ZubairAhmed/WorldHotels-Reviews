8fbec0b8c182909873581f93bd3469c96a208619
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-14 23:50:46 +0530

    header moto style imporoved

--- src/scenes/home/headerHome/HeaderHome.js
+++ src/scenes/home/headerHome/HeaderHome.js
@@ -25,37 +25,35 @@ export default class HeaderHome extends Component {
 
   render() {
     return (
-      <Grid>
-        <div className="headerHome-root">
-          <Row>
-            <Col>
-              <h2 className="headerHome-moto">Where will you travel next?</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={6} lgOffset={3} sm={10} smOffset={1} xs={10} xsOffset={1}>
-              <FormControl
-                className="headerHome-search"
-                placeholder="Find hotels"
-                bsSize="large"
-                value={this.state.searchKey}
-                onChange={this.handleChange}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Button
-                className="headerHome-search-button"
-                size="huge"
-                color="teal"
-                onClick={this.startSearch}
-              >
-                <Glyphicon glyph="search" /> Search
-              </Button>
-            </Col>
-          </Row>
-        </div>
+      <Grid className="headerHome-root">
+        <Row>
+          <Col>
+            <h2 className="headerHome-moto">Where will you travel next?</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={6} lgOffset={3} sm={10} smOffset={1} xs={10} xsOffset={1}>
+            <FormControl
+              className="headerHome-search"
+              placeholder="Find hotels"
+              bsSize="large"
+              value={this.state.searchKey}
+              onChange={this.handleChange}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              className="headerHome-search-button"
+              size="huge"
+              color="teal"
+              onClick={this.startSearch}
+            >
+              <Glyphicon glyph="search" /> Search
+            </Button>
+          </Col>
+        </Row>
       </Grid>
     );
   }
