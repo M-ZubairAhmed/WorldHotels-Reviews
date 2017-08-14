4701fa1c0e2bb2595318551a0890c480efd5edcc
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-14 20:45:50 +0530

    card made same as semantic ui except they are now in center

--- src/components/poster/Poster.js
+++ src/components/poster/Poster.js
@@ -4,18 +4,21 @@ import './poster.css';
 export default class Poster extends Component {
   render() {
     return (
-      <div className="root-poster" onClick={() => console.log('hello')}>
+      <div className="poster-root" onClick={() => console.log('hello')}>
         <img
-          className="image"
+          className="poster-image"
           alt={this.props.hotelTypeTitle}
           src={this.props.hotelTypeImage}
         />
         <h2>
           {this.props.hotelTypeTitle}
         </h2>
-        <h5>
+        <h5 className="poster-gist">
           {this.props.hotelTypeGist}
         </h5>
+        <h4 className="poster-viewButton">
+          <hr />View
+        </h4>
       </div>
     );
   }
