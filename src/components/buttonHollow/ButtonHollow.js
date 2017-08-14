92365b31de344b43b84f5fae2b343e8ce4c076b6
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-08-14 23:17:13 +0530

    hollowbutton css added

--- src/components/buttonHollow/ButtonHollow.js
+++ src/components/buttonHollow/ButtonHollow.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import './buttonHollow.css';
 
 export default class ButtonHollow extends Component {
   render() {
